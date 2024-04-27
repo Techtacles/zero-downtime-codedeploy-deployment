b2d2c523b775c021e6a151dc559e6212632d3e7d
Techtacles <emmanuel.offisong@data2bots.com>
2024-04-27 22:56:58 +0100

    Modifying userdata

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,11 +1,8 @@
 sudo yum update
 sudo yum install ruby
 sudo yum install wget
-CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
-$CODEDEPLOY_BIN stop
-yum erase codedeploy-agent -y
 cd /home/ec2-user
-wget https://bucket-name.s3.region-identifier.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
-# systemctl start codedeploy-agent
+systemctl start codedeploy-agent
