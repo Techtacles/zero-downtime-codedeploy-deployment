259195b32b84f0f304893c07a747a2cb6c9cf5ec
Techtacles <emmanuel.offisong@data2bots.com>
2024-04-27 18:38:08 +0100

    Adding s3 permissions to codedeploy

--- infrastructure/modules/cicd/codedeploy/iam.tf
+++ infrastructure/modules/cicd/codedeploy/iam.tf
@@ -19,7 +19,43 @@ resource "aws_iam_role" "codedeploy_role" {
   assume_role_policy = data.aws_iam_policy_document.codedeploy_assume_role.json
 }
 
+data "aws_iam_policy_document" "codedeploy_policy_doc" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "s3:*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs",
+    ]
+
+    resources = ["*"]
+  }
+
+}
 resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
   role       = aws_iam_role.codedeploy_role.name
+}
+
+resource "aws_iam_role_policy" "codedeploy_policy" {
+  role   = aws_iam_role.codedeploy_role.name
+  policy = data.aws_iam_policy_document.codedeploy_policy_doc.json
 }
