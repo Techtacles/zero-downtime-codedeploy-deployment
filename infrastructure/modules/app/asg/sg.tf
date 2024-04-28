3d538a0818935c417dad5234a5b092e0d9bcecb3
Techtacles <emmanuel.offisong@data2bots.com>
2024-04-28 18:05:16 +0100

    Modifying ec2 sg

--- infrastructure/modules/app/asg/sg.tf
+++ infrastructure/modules/app/asg/sg.tf
@@ -7,6 +7,13 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [var.alb_sg_id]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
