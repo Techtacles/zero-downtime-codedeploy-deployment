61e7dcf34ca4609b77ea5c1c8519a601a7df35c4
Techtacles <emmanuel.offisong@data2bots.com>
2024-04-28 21:29:59 +0100

    Removing alb

--- infrastructure/modules/app/asg/sg.tf
+++ infrastructure/modules/app/asg/sg.tf
@@ -1,11 +1,11 @@
 resource "aws_security_group" "ec2_sg" {
   name = "zero-downtime-deployment-ec2-sg"
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [var.alb_sg_id]
-  }
+  #   ingress {
+  #     from_port       = 80
+  #     to_port         = 80
+  #     protocol        = "tcp"
+  #     security_groups = [var.alb_sg_id]
+  #   }
 
   ingress {
     from_port   = 80
